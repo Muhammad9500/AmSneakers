a32b28e6d7edada19ee3dbdcffde9e42ef908480
Muhammad9500 <84620962+Muhammad9500@users.noreply.github.com>
2022-07-31 13:38:29 +0300

    added changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,14 @@ import Header from "./header/Header"
 import Screen from './screen/Screen';
 import Main from './main/Main';
 import Footer from './footer/Footer';
-import ModalCheckout from './modalCheckout/modalCheckout';
 import "./App.css"
 const App = () => {
-  const initialState = {
-    name: "",
-    comWay: "",
-    number: "",
-    textarea: ""
-  }
-  const [modalCheckout, setModalCheckout] = useState(false);
-  const [formState, setFormState] = useState({ ...initialState });
-  const classForModalCheckout = modalCheckout ? "open" : "close"
 
   return (
   <>
     <Screen >
       <Header/>
-      <Main setModalCheckout={setModalCheckout}>
-      
-        </Main>
-        <ModalCheckout modalCheckout={modalCheckout} classForModalCheckout={classForModalCheckout} setModalCheckout={setModalCheckout} initialState={initialState} formState={formState} setFormState={setFormState}/>
+      <Main/>
       <Footer/>
     </Screen>
   </>
